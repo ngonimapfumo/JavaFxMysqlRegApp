3c9e0cf3ace90f3dd6ef72fc1867d09367ba7e67
Ngoni Mapfumo <ngonimapfumo@hotmail.com>
2020-05-30 12:12:02 +0200

    init commit

--- src/main/java/RegisterController.java
+++ src/main/java/RegisterController.java
@@ -1,12 +1,45 @@
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Window;
+
+import java.awt.event.ActionEvent;
 
 public class RegisterController {
 
     @FXML
-    public void register(){}
+    private TextField fullNameField;
+    @FXML
+    private TextField emailIdField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Button submitButton;
 
 
+    @FXML
+    public void register(ActionEvent actionEvent) {
+        Window window = submitButton.getScene().getWindow();
 
+        if(fullNameField.getText().isEmpty()){
+            Alert(Alert.AlertType.ERROR,window,"Error","Name cannot be empty");
+            return;
+        }
+
+    }
+
+
+    private void Alert(Alert.AlertType alertType, Window window, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.initOwner(window);
+        alert.show();
+
+    }
 
 
 }
