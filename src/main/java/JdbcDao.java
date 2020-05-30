3fa6106bbb64a9aed867ff535973c9603c5a6851
Ngoni Mapfumo <ngonimapfumo@hotmail.com>
2020-05-30 22:44:34 +0200

    added: validation

--- src/main/java/JdbcDao.java
+++ src/main/java/JdbcDao.java
@@ -1,7 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class JdbcDao {
 
@@ -9,6 +6,34 @@ public class JdbcDao {
     private static final String DATABASE_USERNAME = "root";
     private static final String DATABASE_PASSWORD = "root";
     private static final String INSERT_QUERY = "INSERT INTO registration (full_name, email_id, password) VALUES (?, ?, ?)";
+    private static final String SELECT_QUERY = "SELECT * FROM registration WHERE email_id = ? and password = ?";
+
+
+    public boolean validate(String email,String password){
+
+        try (Connection connection = DriverManager
+                .getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD)) {
+
+            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_QUERY);
+            preparedStatement.setString(2, email);
+            preparedStatement.setString(3, password);
+            System.out.println("prepared statement::" + preparedStatement);
+
+            preparedStatement.executeUpdate();
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                return true;
+            }
+
+        } catch (SQLException e) {
+            //todo: put alert with message
+            System.out.println(e.getMessage() + e.getSQLState() + e.getErrorCode());
+        }
+
+        return false;
+    }
+
 
     public void insertData(String name, String email, String password) throws SQLException {
 
